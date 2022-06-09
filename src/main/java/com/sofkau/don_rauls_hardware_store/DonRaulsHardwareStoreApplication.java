a08df85553496e6744cb7c5acf1922f5f47b104c
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-06-09 12:46:51 -0300

    ADD --- Empty Spring Boot project with some dependencies

---
+++ src/main/java/com/sofkau/don_rauls_hardware_store/DonRaulsHardwareStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.don_rauls_hardware_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonRaulsHardwareStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonRaulsHardwareStoreApplication.class, args);
+	}
+
+}
